49189cd1d792e59e9b218631d7b651d7d35206ff
TheBradJones <thebradjones06@googlemail.com>
2026-05-31 16:45:29 +0100

    Dont break this time

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController characterController;
+    private Camera playerCamera;
 
     public float MovementSpeed = 10f, RotationSpeed = 5f, jumpForce = 10f, gravity = -30f;
 
@@ -14,17 +15,30 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+
+        if (playerCamera == null)
+            playerCamera = Camera.main;
     }
 
     public void Move(Vector2 movementVector)
     {
         // Basic WASD Movement
-        Vector3 move = transform.forward * movementVector.y + transform.right * movementVector.x;
-        move = move * MovementSpeed * Time.deltaTime;
-        characterController.Move(move);
+        Vector3 camForward = playerCamera.transform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+
+        Vector3 camRight = playerCamera.transform.right;
+        camRight.y = 0;
+        camRight.Normalize();
+
+        Vector3 move = camForward * movementVector.y + camRight * movementVector.x;
+        characterController.Move(move * MovementSpeed * Time.deltaTime);
+
+        if (characterController.isGrounded && verticalVelocity < 0)
+            verticalVelocity = -2f;
 
         // Basic Jump Movement
-        verticalVelocity = verticalVelocity + gravity * Time.deltaTime;
+        verticalVelocity +=  gravity * Time.deltaTime;
         characterController.Move(new Vector3(0, verticalVelocity, 0) * Time.deltaTime);
     }
 
@@ -36,11 +50,11 @@ public class PlayerMovement : MonoBehaviour
 
         Vector2 rotation = new Vector2(Mathf.Clamp(rotX, -85, 85), rotY);   // Clamps up/down rotation
 
-        transform.localRotation = Quaternion.Euler(rotation.x, rotation.y, 0);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotation.x, rotation.y, 0);
     }
 
     public void Jump()
-    {
+    {   
         if (characterController.isGrounded)
         {
             verticalVelocity = jumpForce;
